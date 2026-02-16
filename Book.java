3050ec3ea15f414eabca51190636327aa0efee2a
brennenlui <66094475+brennenlui@users.noreply.github.com>
2026-02-16 09:45:57 -0500

    Exercise 2.83

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Brennen Lui
+ * @version 2/16/2026
  */
 class Book
 {
@@ -23,4 +23,11 @@ class Book
     }
 
     // Add the methods here ...
+    public String getAuthor(){
+        return author;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
 }
